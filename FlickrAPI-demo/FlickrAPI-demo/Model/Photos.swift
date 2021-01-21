5a71cb8b09b0e46a29651d52292a87fd73536cfc
FrAndrey <aparamon@my.centennialcollege.ca>
2021-01-21 15:29:04 +0700

    Photo - decodable Model added

---
+++ FlickrAPI-demo/FlickrAPI-demo/Model/Photos.swift
@@ -0,0 +1,42 @@
+//
+//  Photos.swift
+//  FlickrAPI-demo
+//
+//  Created by Andrey on 2021-01-21.
+//  Copyright © 2021 Andrey. All rights reserved.
+//
+
+import UIKit
+
+
+struct Response: Decodable {
+    
+    let photos:Photos
+}
+
+  struct Photos: Codable{
+        
+        //collection of the general information about photos
+        let page:Int
+        let pages:Int
+        let perpage:Int
+        let photo:[Photo]
+        }
+    
+    struct Photo : Codable{
+        
+        //main parameters for grabbing the url for each photo
+        // link is of the following format var
+        // https://live.staticflickr.com/{server-id}/{id}_{secret}_c.jpg
+        
+        let id: String
+        let secret: String
+        let server: String
+        
+        enum CodingKeys: String, CodingKey {
+            case id = "id"
+            case secret = "secret"
+            case server = "server"
+        }
+    }
+

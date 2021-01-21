0511303fdc45ef6ac42df659997523560cd8e715
FrAndrey <aparamon@my.centennialcollege.ca>
2021-01-21 15:04:40 +0700

    XCode Electrolux project initial commit by Andrey

---
+++ FlickrAPI-demo/FlickrAPI-demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickrAPI-demo
+//
+//  Created by Andrey on 2021-01-21.
+//  Copyright © 2021 Andrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

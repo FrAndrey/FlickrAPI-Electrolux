7fa1d8d28fc0b687bde7fcf39ed1942325f99bd4
FrAndrey <aparamon@my.centennialcollege.ca>
2021-01-23 01:22:23 +0700

    Biography is done, BUT with massive view controller. Refactoring needed ( rearrenge -> view & viewModel)

---
+++ FlickrAPI-demo/FlickrAPI-demo/View/Biography.swift
@@ -0,0 +1,20 @@
+//
+//  Biography.swift
+//  FlickrAPI-demo
+//
+//  Created by Andrey on 2021-01-22.
+//  Copyright © 2021 Andrey. All rights reserved.
+//
+
+import UIKit
+
+//view for Biography tab
+class Biography {
+    
+    
+    
+    
+    
+    
+    
+}
